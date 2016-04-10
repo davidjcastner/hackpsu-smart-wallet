2aa6749b8208ae32e0787ac933d66dd4c7891e63
David Castner <davidjcastner@gmail.com>
2016-04-09 21:59:56 -0400

    save point

--- app/client/mdlComponentFix.js
+++ app/client/mdlComponentFix.js
@@ -1,6 +1,9 @@
+import { Blaze } from 'meteor/blaze';
+
 _.each(_.keys(Template), function(key) {
      if (Template[key] instanceof Blaze.Template) {
         //console.log(key, "is the name of a template");
-        Template[key].onRendered(componentHandler.upgradeAllRegistered());
+        //console.log(key);
+        //Template[key].onRendered(componentHandler.upgradeAllRegistered());
      }
 });
