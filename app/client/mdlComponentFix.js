a09dc71b9cbf45ccfae9960699b604c6009005d3
David Castner <davidjcastner@gmail.com>
2016-04-09 21:50:32 -0400

    edit mdlfix

--- app/client/mdlComponentFix.js
+++ app/client/mdlComponentFix.js
@@ -1,6 +1,6 @@
 _.each(_.keys(Template), function(key) {
      if (Template[key] instanceof Blaze.Template) {
         //console.log(key, "is the name of a template");
-        Template[key].onRendered(componentFix);
+        Template[key].onRendered(componentHandler.upgradeAllRegistered());
      }
 });
