357798d874754859875441248350aecb4fe4675c
FeiHong Ren <fwr5057@gmail.com>
2016-04-10 02:16:15 -0400

    email

--- app/client/client.js
+++ app/client/client.js
@@ -1,3 +1,12 @@
-Meteor.call('sendEmail', 'fwr5057@psu.edu', 'psusmartwallet@gmail.com', 'Hello from Meteor!', 'This is a test of Email.send.');
+var emailFrom = "fwr5057@psu.edu";
+var emailTo = "psusmartwallet@gmail.com";
+var subject = "Smart Wallet Alert";
+var messagePart1 = "Hi, \n	You account have a suspicious activity of $"
+var transactionAmount = "0";
+var messagePart2 = " at "
+var transactionPlace = "Penn State";
+var completeMessage = messagePart1.concat(transactionAmount, messagePart2, transactionPlace);
+
+Meteor.call('sendEmail', emailFrom, emailTo, subject, completeMessage);
 
 // this file is for general client code
--- app/server/email.js
+++ app/server/email.js
@@ -1,9 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
 Meteor.startup(function () {
     //  the MAIL_URL environment variable should be in the form of:
     //  smtp://USERNAME:PASSWORD@HOST:PORT/
     var smtpSettings = {
-        USERNAME: "postmaster@mg.smartwallet.com", // SMTP Hostname
-        PASSWORD: "5e548392144a5255deca9ce486df1d11", // Default Password
+        USERNAME: "postmaster@sandboxbe50ea0bf4474274ab31df7579302a07.mailgun.org", // SMTP Hostname
+        PASSWORD: "64bddce0beabdd026f93d9ab93e294c8", // Default Password
         HOST: "smtp.mailgun.org",
         PORT: "587"
     }
@@ -14,4 +16,24 @@ Meteor.startup(function () {
     smtpString += "@" + smtpSettings.HOST + ":" + smtpSettings.PORT + "/";
     //console.log(smtpString);
     process.env.MAIL_URL = smtpString;
+});
+
+
+
+// In your server code: define a method that the client can call
+Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+//    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
 });
